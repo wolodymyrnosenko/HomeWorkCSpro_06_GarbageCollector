a40c5766c9935904fc7c6a9361553da19bb572e7
volodymyr <vovanose@ukr.net>
2025-06-03 11:01:48 +0300

    Add task 06_1 GC Play and Destructor: ~Play

---
+++ HomeWorkCSpro_06_1_GC_PlayDestructor/Play.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HomeWorkCSpro_06_1_GC_PlayDestructor
+{
+    internal class Play
+    {
+        public string Name {get; private set; }
+        public string Author {get; private set; }
+        public string Genre {get; private set; }
+        public int Year {get; private set; }
+
+        public Play(string name, string author, string genre, int year)
+        {
+            Name = name;
+            Author = author;
+            Genre = genre;
+            Year = year;
+        }
+        ~Play()
+        {
+            Console.WriteLine($"Destructor for {this.Name} is processing");
+        }
+        public override string ToString()
+        {
+            return $"{Environment.NewLine}Назва п'єси: {Name}" +
+                $"{Environment.NewLine}Автор: {Author}" +
+                $"{Environment.NewLine}Жанр: {Genre}" +
+                $"{Environment.NewLine}Рік: {Year}{Environment.NewLine}";
+        }
+    }
+}
---
+++ HomeWorkCSpro_06_1_GC_PlayDestructor/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace HomeWorkCSpro_06_1_GC_PlayDestructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Play testPlay1 = new Play("namePlay1", "authorPlay1", "genrePlay1", 2000);
+            Play testPlay2 = new Play("namePlay2", "authorPlay2", "genrePlay2", 2010);
+            Console.WriteLine(testPlay1);
+            Console.WriteLine(testPlay2);
+            testPlay1 = null;//To demonstrate Destructor. Output: nothing
+            //testPlay2 = null;//To demonstrate Destructor
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            Console.ReadLine();
+        }
+    }
+}
